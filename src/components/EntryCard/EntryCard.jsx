aa373ee50fd56dc185bfc9bd880397b6d423c5f4
Jonit <jonitowt@gmail.com>
2023-12-18 15:07:29 +0800

    entries page set up

--- src/components/EntryCard/EntryCard.jsx
+++ src/components/EntryCard/EntryCard.jsx
@@ -1,9 +1,11 @@
-export default function EntryCard() {
+export default function EntryCard({ id, title, body, mood, phase, date }) {
   return (
     <>
-      <h2>title</h2>
-      <p>journal entry</p>
-      <p>mood</p>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <p>{mood}</p>
+      <p>{phase}</p>
+      <p>{date}</p>
     </>
   );
 }
--- src/pages/EntriesPage/EntriesPage.jsx
+++ src/pages/EntriesPage/EntriesPage.jsx
@@ -1,14 +1,39 @@
 import EntryCard from "../../components/EntryCard/EntryCard";
 import { useEffect, useState } from "react";
 
-export default function EntriesPage() {
-  //   const [entries, setEntries] = useState([]);
+const token =
+  "patKTXIqrZPZMbcrF.d8be823da8d1aeff586598a1f97f961cf33ba3de31cc4a0e03c2f7962ea0989a";
 
-  useEffect;
+export default function EntriesPage({ entriesList }) {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const url = "https://api.airtable.com/v0/appRIGgG5hdxdDksC/Table%201";
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const entriesData = await response.json();
+      setEntries(entriesData.records);
+    })();
+  }, []);
+
   return (
     <>
       <h2>entries page</h2>
-      <EntryCard />
+      {entries.map((entry) => (
+        <EntryCard
+          key={entry.id}
+          title={entry.fields.title}
+          body={entry.fields.body}
+          mood={entry.fields.mood}
+          phase={entry.fields.phase}
+          date={entry.fields.date}
+        />
+      ))}
     </>
   );
 }
