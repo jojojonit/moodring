d4431ba79d23aea71a1f93c5622a95fe2536b17c
Jonit <jonitowt@gmail.com>
2023-12-19 21:20:45 +0800

    moonphase now updates when new date is selected

--- src/pages/MoonPage/MoonPage.jsx
+++ src/pages/MoonPage/MoonPage.jsx
@@ -1,52 +1,51 @@
 import { useEffect, useState } from "react";
-import MoonPhase from "../../components/MoonPhase/MoonPhase";
 
 const token =
   "NzM0M2M2ZDEtNzNjMS00NWI2LWIzMzQtZDE0YzIwNTNjMzljOmEyZmY2MGQ5YmFmMWYyMTZlZGZlMTNhNTNkYTY3MmI1MWZhOWNjZWZhZDllZTk1ZTUzZTc2NmVkZTBiMjI4ZDkxNjM4MmNiNTllZjJmZTFmOWE2ODViM2Q2ZTYzNDFkYzJlMWFlZmMzMzVjMTJkMGNkYWI2YWQ4YjkyNjMxNTdmNzgyZDFkNTYxNmU0N2ExNzg5ZmMyYTg1M2RmNWRkNWNhZmE2ZTA2NzE5ZjY4ZmRlOWNmOGQ1YTNlNmM5OTUyYjExZGIyNjYwNjk5YjljMDYzZmNmNmQ1ZWZlYWE0NGI0";
+
+const fetchMoonPhase = async (startDate, setPhase) => {
+  const url = "https://api.astronomyapi.com/api/v2/studio/moon-phase";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      format: "png",
+      style: {
+        moonStyle: "default",
+        backgroundStyle: "solid",
+        backgroundColor: "transparent",
+        headingColor: "transparent",
+        textColor: "black",
+      },
+      observer: {
+        latitude: 6.56774,
+        longitude: 79.88956,
+        date: startDate,
+      },
+      view: {
+        type: "landscape-simple",
+        orientation: "south-up",
+      },
+    }),
+  });
+  const moonPhaseData = await response.json();
+  setPhase(moonPhaseData.data);
+};
 
 export default function MoonPage({ startDate }) {
   const [phase, setPhase] = useState({});
 
   useEffect(() => {
-    const fetchMoonPhase = async () => {
-      const url = "https://api.astronomyapi.com/api/v2/studio/moon-phase";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          format: "png",
-          style: {
-            moonStyle: "default",
-            backgroundStyle: "solid",
-            backgroundColor: "transparent",
-            headingColor: "transparent",
-            textColor: "black",
-          },
-          observer: {
-            latitude: 6.56774,
-            longitude: 79.88956,
-            date: startDate,
-          },
-          view: {
-            type: "landscape-simple",
-            orientation: "south-up",
-          },
-        }),
-      });
-      const moonPhaseData = await response.json();
-      setPhase(moonPhaseData.data);
-    };
-    fetchMoonPhase();
-  }, []);
+    fetchMoonPhase(startDate, setPhase);
+  }, [startDate]);
 
   return (
     <>
       <h2>moonpage</h2>
       <img src={phase.imageUrl} alt="Moon Phase" />
-      {/* <MoonPhase phase={phase} /> */}
     </>
   );
 }
